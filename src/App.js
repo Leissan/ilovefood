9c1c35eec8f563ce06d9180c19784d01eb69e87b
Leissan Khan <leissan.khan@gmail.com>
2022-07-02 18:39:28 -0400

    new

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react"
 import Home from "./components/static/Home";
+import Navbar from "./components/navigation/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ListRecipes from "./components/recipes/ListRecipes";
+import NewRecipe from "./components/recipes/NewRecipe";
 
 const App = () => {
   return (
-    <div className="App">
-      <Home />
-
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/recipes" element={<ListRecipes />} />
+        <Route path="/recipes/new" element={<NewRecipe />} />
+        <Route exact path="/" element={<Home />} />
+      </Routes>  
+    </Router>
     
-    </div>
   );
 }
 
